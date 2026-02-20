c229c76fa49acf45a2ee0c3ef1b144cda3ae51ef
ninhhoang2005 <nguyenninhhoang2005@gmail.com>
2026-02-20 21:14:36 +0700

    new feature
    after it some new feature:
    - add play tab control:
      - enter video link: you can paste link in edit box.
      - play button: activate play video from the link.
      - download button: activate download frome the link. when press, you can find one submenu or maybe say context menu.
    - add detect link from clipboard:
      - when you have link from clipboard, one dialog show when you run application.
    - in dialog:
      - play button
      - download button.
    - fix built commands with nuitka.
    - all window console have been disable.
    you can try and report if find error.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 title "start building"
-uv run -m nuitka --standalone --windows-console=disable --remove-output -o "te_tube.exe" main.py
+uv run -m nuitka --standalone --windows-disable-console --remove-output -o "te_tube.exe" main.py
 xcopy "lib" "main.dist/lib" /E /I /Y
 cls
 title "Build successful"
